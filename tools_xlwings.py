08123a91ec36b617e627e574dde1d1587c5bb162
colderleo <colderleo@163.com>
2020-09-25 14:14:57 +0800

    add xlwings

--- tools_xlwings.py
+++ tools_xlwings.py
@@ -1,4 +1,6 @@
 import xlwings
+
+
 def find_xlwings_cell(rng:xlwings.Range, target, startswith=False):
     'cell is a 1x1 Range. 非常慢'
     for row in rng.rows:
@@ -20,25 +22,59 @@ def get_xlwings_total_range(sheet:xlwings.Sheet) -> xlwings.Range:
     return sheet[:sheet.used_range.last_cell.row, :sheet.used_range.last_cell.column]
 
 
-def xlwings_demo():
-    import xlwings
+def insert_value_by_mark(xls_filename, row_mark, col_mark, value, sheet_name=None):
+    'inster value buy finding row_mark and col_mark.'
+    import pyexcel_xls
+    from tools_xls import find_row_col
 
-    app = xlwings.apps.active
+    xls_total_data = pyexcel_xls.get_data(xls_filename)
+    if sheet_name is None:
+        xls_data = xls_total_data[next(iter(xls_total_data))]
+    else:
+        xls_data = xls_total_data[sheet_name]
+
+    tar_rowi, _col = find_row_col(xls_data, row_mark)
+    if tar_rowi is None:
+        raise Exception(f"'{xls_filename}'内找不到{row_mark}")
+    _row, tar_coli = find_row_col(xls_data, col_mark)
+    if tar_coli is None:
+        raise Exception(f"'{xls_filename}'内找不到{col_mark}")
+        
+    # workbook = xlwings.Book(xls_filename)
+    workbook = app.books.open(xls_filename, update_links=True)
+    if sheet_name is None:
+        sheet:xlwings.Sheet = workbook.sheets[0]
+    else:
+        sheet:xlwings.Sheet = workbook.sheets[sheet_name]
+    tar_cell = sheet[tar_rowi, tar_coli]
+    tar_cell.value = value
+    workbook.save()
+    print(f'    {xls_filename} - {row_mark} - {col_mark} - {value} 写入成功')
+
+
+def get_or_create_xlwings_app():
+    app = xlwings.apps.active #优先使用现有的excel app实例，避免创建新的app导致冲突
     if not app:
         app = xlwings.App(visible=True, add_book=False)
         print('create new excel app')
     app.visible = True
-    app.display_alerts = False
-    app.books.open('xxx.xlsx') #这样会使用现有的excel app实例，不会创建新的导致冲突
-    
+    app.display_alerts = True #如果设置为False 文件修改后关闭会不提示保存
+    return app
+
+
+def xlwings_demo():
+    app = get_or_create_xlwings_app()
+
+    wb = app.books.open('xxx.xlsx') #open an existing xls. if it's already opened, will connect, not reopen.
     wb = xlwings.Book()  # this will create a new workbook
-    # wb = xlwings.Book('FileName.xlsx')  # connect to an existing file in the current working directory
+
     sheet:xlwings.Sheet = wb.sheets[0]  # sheet = wb.sheets['Sheet1']
     max_row = sheet.used_range.last_cell.row
     max_col = sheet.used_range.last_cell.column
 
-    fill_ref_range = sheet[0, 3:max_col]
-    fill_full_range = sheet[0:max_row, 0:max_col]
+    # 下拉自动填充
+    fill_ref_range = sheet[0, 3:max_col] #填充参考区域
+    fill_full_range = sheet[0:max_row, 0:max_col] #要填充的所有区域
     # sheet.range('B103:AQ103').api.AutoFill(sheet.range('B103:AQ104').api,0)
     fill_ref_range.api.AutoFill(fill_full_range.api, 0)
 
